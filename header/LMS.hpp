0c667f26c3c919ae8e3f72c723dba39f3710198a
Campionnn <rchie011@ucr.edu>
2023-05-16 17:49:15 -0700

    Renamed searchBase list & added funciton hide echo - renamed searchBase list to searchBaseList to avoid overwriting the class - added setStdInEcho funciton to hide echo when entering password

--- header/LMS.hpp
+++ header/LMS.hpp
@@ -8,11 +8,12 @@ class LMS {
     private:
         User currentUser;
         list<Book> bookDatabase;
-        list <searchBase> searchBase;
+        list<searchBase> searchBaseList;
     public:
         LMS();
         void welcomePrompt();
         void signUpPrompt();
+        void setStdInEcho(bool);
         void loginPrompt();
         void mainMenuPrompt();
         void logoutPrompt();
