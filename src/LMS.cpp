c1d821bd9d13e75fbcf8d01699308e55d9dcfe3f
Campionnn <rchie011@ucr.edu>
2023-05-16 17:51:52 -0700

    finished signUpPrompt - changed welcomePrompt to also get user input and go to corresponding screen - fixed include to properly include LMS header file -

--- src/LMS.cpp
+++ src/LMS.cpp
@@ -1,11 +1,22 @@
-#include "LMS.hpp"
 #include <iostream>
+#include <string>
+
+#include "../header/LMS.hpp"
+
+// used to disable password echo
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+#endif
 
 LMS::LMS() {
-    this->welcomePrompt();
+    welcomePrompt();
 }
 
 void LMS::welcomePrompt() {
+    std::string input;
     std::cout   << "\t\tWelcome to the UC Riverside Library!" << std::endl
                 << "\tWith an account at our online library, you will be able to:" << std::endl
                 << "- Have access to a plethora of books and resources" << std::endl
@@ -16,10 +27,59 @@ void LMS::welcomePrompt() {
                 << std::endl
                 << "Press L to log into an existing account" << std::endl
                 << "Press S to sign up for a new account" << std::endl;
+    std::cin >> input;
+    if (input == "L" || input == "l") {
+        loginPrompt();
+    } else if (input == "S" || input == "s") {
+        signUpPrompt();
+    } else {
+        std::cout << "Invalid input. Please try again" << std::endl;
+        welcomePrompt();
+    }
 }
 
 void LMS::signUpPrompt() {
+    std::string name;
+    std::string username;
+    std::string password;
+    std::cout   << "\t\t\tRegister" << std::endl
+                << "Please fill out the following fields so we may create an account for you" << std::endl
+                << "Full Name: ";
+    std::cin.ignore();
+    std::getline(std::cin, name);
+    std::cout   << "Username: ";
+    std::getline(std::cin, username);
+    std::cout   << "Password: ";
+    setStdInEcho(false);
+    std::getline(std::cin, password);
+    setStdInEcho(true);
+    std::cout   << std::endl;
+}
 
+// stolen from https://stackoverflow.com/questions/1413445/reading-a-password-from-stdcin
+void LMS::setStdInEcho(bool enable = true) {
+    #ifdef WIN32
+        HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE); 
+        DWORD mode;
+        GetConsoleMode(hStdin, &mode);
+
+        if( !enable )
+            mode &= ~ENABLE_ECHO_INPUT;
+        else
+            mode |= ENABLE_ECHO_INPUT;
+
+        SetConsoleMode(hStdin, mode );
+
+    #else
+        struct termios tty;
+        tcgetattr(STDIN_FILENO, &tty);
+        if( !enable )
+            tty.c_lflag &= ~ECHO;
+        else
+            tty.c_lflag |= ECHO;
+
+        (void) tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+    #endif
 }
 
 void LMS::loginPrompt() {
