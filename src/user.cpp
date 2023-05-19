64b034b0e2f1b37f15865d384849ab096be3e61c
Augustus-Caesar886 <williambruce398@gmail.com>
2023-05-18 17:26:06 -0700

    Add implementations to certain methods for user

--- src/user.cpp
+++ src/user.cpp
@@ -1,37 +1,48 @@
-#include "user.hpp"
+#include "../header/user.hpp"
+#include <functional>
 
-User::User() {
-
+User::User() : username("Guest"), hashedPassword(0), fine(0) {
 }
 
-string User::getUsername() {
-
+User::User(const string& username, const string& password) : username(username), fine(0) {
+    hashedPassword = hash<string>{}(password);
 }
 
-string User::hashPassword() {
-
+string User::getUsername() const {
+    return username;
 }
 
-double User::getFine() {
-
+int User::hashPassword() const {
+    return hashedPassword;
 }
 
-void User::setUsername(string username) {
+double User::getFine() const {
+    return fine;
+}
 
+void User::setUsername(const string& username) {
+    this->username = username;
 }
 
 void User::setFine(double fine) {
-
+    this->fine = fine;
 }
 
 void User::removeBook(Book returnedBook) {
-
+    //THIS IS A STUB
+    return;
 }
 
 void User::addBook(Book checkoutBook) {
-
+    //THIS IS A STUB
+    return;
 }
 
 void User::displayMenu() {
+    //THIS IS A STUB
+    return;
+}
 
+const list<Book>& User::getBooks() const {
+    return checkedOutBooks;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,72 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../header/user.hpp"
+#include "../header/book.hpp"
 
 using ::testing::_;
 using ::testing::InSequence;
+using namespace std;
+
+//To be removed when Book is implemented
+bool operator==(const Book&, const Book&) {
+    return false;
+}
 
 TEST(googletestSetup, expect0) {
     EXPECT_EQ(0, 0);
+}
+
+TEST(userTests, testGuestUser) {
+    User defaultUser;
+    EXPECT_EQ(defaultUser.getUsername(), "Guest");
+    EXPECT_EQ(defaultUser.hashPassword(), 0);
+}
+
+TEST(userTests, testExampleUserLogin) {
+    const string username = "JohnDoe47", password = "password";
+    User exampleUser(username, password);
+    EXPECT_EQ(exampleUser.getUsername(), username);
+    EXPECT_EQ(exampleUser.hashPassword(), (int)hash<string>{}(password));
+}
+
+TEST(userTests, testDefaultFine) {
+    User defaultUser;
+    EXPECT_DOUBLE_EQ(defaultUser.getFine(), 0.0);
+}
+
+TEST(userTests, testCustomFine) {
+    User exampleUser("", "");
+    EXPECT_DOUBLE_EQ(exampleUser.getFine(), 0.0);
+}
+
+TEST(userTests, testUsernameMethods) {
+    User exampleUser("", "");
+    const string username = "JohnDoe47";
+    exampleUser.setUsername(username);
+    EXPECT_EQ(exampleUser.getUsername(), username);
+}
+
+TEST(userTests, testFineMethods) {
+    User defaultUser;
+    defaultUser.setFine(defaultUser.getFine() + 5);
+    EXPECT_DOUBLE_EQ(defaultUser.getFine(), 5);
+}
+
+TEST(userTests, testBookAdd) {
+    User defaultUser;
+    Book b;
+    defaultUser.addBook(b);
+    const list<Book>& books = defaultUser.getBooks();
+    EXPECT_EQ(books.size(), 1);
+    EXPECT_EQ(books.front(), b);
+}
+
+TEST(userTests, testBookRemove) {
+    User defaultUser;
+    Book b1, b2; //To be updated when Book is implemented
+    defaultUser.addBook(b1);
+    defaultUser.addBook(b2);
+    defaultUser.removeBook(b1);
+    EXPECT_EQ(defaultUser.getBooks().size(), 1);
+    EXPECT_EQ(defaultUser.getBooks().front(), b2);
 }
